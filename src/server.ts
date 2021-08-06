21f8c4450323f3d44f2b42a20a428b4a43578c05
marco <marcoavfcc@gmail.com>
2021-08-05 22:21:30 -0300

    initial swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
-import { app } from ".";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+
+import swaggerFile from "./swagger.json";
+
+// import { app } from ".";
+const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
